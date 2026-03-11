bd61f2a12267a520ea3b46738d538e8450240da5
gabriellopezd <56359531+gabriellopezd@users.noreply.github.com>
2026-03-11 09:31:21 -0500

    Add Certificate model

---
+++ Models/Certificate.cs
@@ -0,0 +1,7 @@
+public class Certificate
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime IssueDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
